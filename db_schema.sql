2b25007dd29bb366a289f38f9f38ac408efedf72
Shrewbi <minichili@hotmail.com>
2017-02-16 15:41:28 +0100

    removed unnecessary

--- db_schema.sql
+++
@@ -1,7 +0,0 @@
-DROP TABLE IMG_BLNGS_TO;
-DROP TABLE STONES;
-DROP TABLE IMAGES;
-
-CREATE TABLE STONES(id int, name varchar(50), lat float, long float, location varchar(50), PRIMARY KEY(id));
-CREATE TABLE IMAGES(id int, path varchar(100), PRIMARY KEY(id));
-CREATE TABLE IMG_BLNGS_TO(stoneid int REFERENCES STONES(id), imgid int REFERENCES IMAGES(id));
