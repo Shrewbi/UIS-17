1630a4e84cdb174134ab44fe3aeaeabcd03de54b
Shrewbi <minichili@hotmail.com>
2017-03-02 16:07:11 +0100

    keys

--- Assignments/Assignment 1/question2.sql
+++ Assignments/Assignment 1/question2.sql
@@ -2,57 +2,68 @@ CREATE TABLE studerende(
   id CHAR(6),
   name VARCHAR,
   birthday DATE,
-  best_friend_id CHAR(6)
+  best_friend_id CHAR(6),
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE venner(
-  studerende_id_1 CHAR(6),
-  studerende_id_2 CHAR(6)
+  studerende_id_1 CHAR(6) REFERENCES studerende(id),
+  studerende_id_2 CHAR(6) REFERENCES studerende(id)
 );
 
 CREATE TABLE kurser(
   id INT,
-  name VARCHAR
+  name VARCHAR,
+  PRIMARY KEY(id)
 );
 
 CREATE TABLE tilmelding(
-  studerende_id CHAR(6),
-  kursus_id INT,
+  studerende_id CHAR(6) REFERENCES studerende(id),
+  kursus_id INT REFERENCES kurser(id),
   vurdering INT,
-  karakter CHAR
-);
+  karakter INT
+ );
 
 CREATE TABLE periode(
-  id INT,
   year INT,
-  block INT
+  block INT,
+  semester BOOLEAN,
+  PRIMARY KEY(year, block, semester)
 );
 
 CREATE TABLE udbud(
   kursus_id INT,
-  periode_id INT
+  year INT,
+  block INT,
+  semester BOOLEAN, /* Reserved for courses streching more than 2 blocks */
+  udbudt BOOLEAN, /* Is the course offered? */
+  FOREIGN KEY(kursus_id) REFERENCES kurser(id),
+  FOREIGN KEY(year)      REFERENCES periode(year),
+  FOREIGN KEY(block)     REFERENCES periode(block),
+  FOREIGN KEY(semester)    REFERENCES periode(semester)
 );
 
 CREATE TABLE undervisere(
-  id INT,
-  email VARCHAR,
-  name VARCHAR
+  email VARCHAR(100),
+  name VARCHAR(100),
+  PRIMARY KEY(email)
 );
 
 CREATE TABLE tilknyttet(
-  kursus_id INT,
-  underviser_id INT
+  kursus_id INT REFERENCES kurser(id),
+  underviser_email VARCHAR(100) REFERENCES undervisere(email)
 );
 
 CREATE TABLE forelæsning(
-  kursus_id INT,
+  forelæsning_id INT,
+  kursus_id INT REFERENCES kurser(id),
   ugenummer INT,
-  uge INT,
+  ugedag INT, /* 1: mandag, 2: tirsdag, etc. */
   start_time TIME,
   end_time TIME
 );
 
 CREATE TABLE ansvarlig(
-  underviser_id INT,
-  forelæsning_id INT
+  underviser_email VARCHAR(100) REFERENCES undervisere(email),
+  forelæsning_id INT REFERENCES forelæsning(forelæsning_id)
 );
