47353187455fc020455064939aaac3e36d98ba98
Shrewbi <minichili@hotmail.com>
2017-03-28 14:39:35 +0200

    product sql schema

--- Product/db_schema.sql
+++ Product/db_schema.sql
@@ -1,7 +1,14 @@
-DROP TABLE IMG_BLNGS_TO;
-DROP TABLE STONES;
-DROP TABLE IMAGES;
+DROP TABLE IF EXISTS Field_blngs_to CASCADE;
+DROP TABLE IF EXISTS Media_blngs_to CASCADE;
+DROP TABLE IF EXISTS Field;
+DROP TABLE IF EXISTS Item;
+DROP TABLE IF EXISTS Media;
 
-CREATE TABLE STONES(id int, name varchar(50), lat float, long float, location varchar(50), PRIMARY KEY(id));
-CREATE TABLE IMAGES(id int, path varchar(100), PRIMARY KEY(id));
-CREATE TABLE IMG_BLNGS_TO(stoneid int REFERENCES STONES(id), imgid int REFERENCES IMAGES(id));
+CREATE TABLE Field(id SERIAL PRIMARY KEY, title char(255), description char(2047), body char(2047));
+CREATE TABLE Item(id SERIAL PRIMARY KEY, lat float NOT NULL, long float NOT NULL);
+CREATE TABLE Field_blngs_to(iid int REFERENCES Item(id), fid int REFERENCES Field(id) ON DELETE CASCADE);
+CREATE TABLE Media(id SERIAL PRIMARY KEY, type char(10) NOT NULL, path char(255) NOT NULL);
+CREATE TABLE Media_blngs_to(mid int REFERENCES Media(id), fid int REFERENCES Field(id) ON DELETE CASCADE);
+
+
+-- How strict are we? NOT NULL everything to prevent errors? Just the most important? nothing? --
