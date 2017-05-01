a9de778a3662e4c853e13161ef9c1c747d6ede38
Benjamin Egelund-Muller <b@egelund-muller.com>
2017-05-01 20:41:22 +0200

    Page + some sign in

--- Product/api.py
+++ Product/api.py
@@ -3,6 +3,11 @@ import database
 
 app = Flask(__name__)
 app.debug = True
+app.secret_key = "314"
+
+@app.route("/")
+def home():
+    return render_template("map.html")
 
 @app.route("/test")
 def test():
@@ -10,3 +15,26 @@ def test():
     rows = database.execute(query)
     res = [{"id" : r[0], "body" : r[1]} for r in rows]
     return jsonify(data=res)
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if credentials_valid(username, password):
+            session['logged_in'] = True
+            print("wow")
+            return redirect('/')
+        else:
+            error = "Invalid credentials"
+    return render_template('login.html', error=error)
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect('/')
+
+def credentials_valid(username, password):
+    return username == "hey"
