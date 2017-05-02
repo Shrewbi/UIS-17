4b4b5cf10e9d8310cb72e0e07ce0940e7a8afba0
Benjamin Egelund-Muller <b@egelund-muller.com>
2017-05-02 17:07:59 +0200

    Whoops, forgot to check this in

--- Product/api.py
+++ Product/api.py
@@ -12,20 +12,3 @@ app.register_blueprint(endpoints.site.bp)
 app.register_blueprint(endpoints.admins.bp)
 app.register_blueprint(endpoints.media.bp)
 app.register_blueprint(endpoints.templates.bp)
-
-#
-# GET /api/items
-#
-# POST /api/items (template_id, x_coordinate, y_coordinate)
-#
-# POST /api/items/id/fields (name, media_id)
-#
-# DELETE /api/items/id/fields/id
-#
-# DELETE /api/items/id
-#
-# GET /api/items/paths
-#
-# POST /api/items/paths from_item_id, to_item_id
-#
-# DELETE /api/items/paths/id
