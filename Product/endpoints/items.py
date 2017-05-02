1128832fe82ec87a5ded86064f4f63d2f289c1a5
Benjamin Egelund-Muller <b@egelund-muller.com>
2017-05-02 17:07:24 +0200

    Added todo for remaining endpoints

---
+++ Product/endpoints/items.py
@@ -0,0 +1,17 @@
+# TODO: Add these endpoints 
+#
+# GET /api/items
+#
+# POST /api/items (template_id, x_coordinate, y_coordinate)
+#
+# POST /api/items/id/fields (name, media_id)
+#
+# DELETE /api/items/id/fields/id
+#
+# DELETE /api/items/id
+#
+# GET /api/items/paths
+#
+# POST /api/items/paths from_item_id, to_item_id
+#
+# DELETE /api/items/paths/id
